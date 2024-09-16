828296b9643caf4eb5c8fdc6884e873b7bda7077
eduardaczza <165608058+eduardaczza@users.noreply.github.com>
2024-09-15 21:35:59 -0300

    primeira aula finalizada

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,8 @@
 import { pgTable, text, integer, timestamp } from "drizzle-orm/pg-core";
 import { createId} from '@paralleldrive/cuid2'
-export const goals = pgTable('goals', {
-    id: text('id').primaryKey().$default(() => createId()),
+
+    export const goals = pgTable('goals', {
+    id: text('id').primaryKey().$defaultFn(() => createId()),
     title: text('title').notNull(),
     desiredWeeklyFrequency: integer('desired_weekly-frequency').notNull(),
     createdAt: timestamp('created_at', {withTimezone: true}).notNull().defaultNow(),
@@ -9,7 +10,7 @@ export const goals = pgTable('goals', {
 } )
 
 export const goalCompletions = pgTable('goal_completions', {
-    id: text('id').primaryKey(),
+    id: text('id').primaryKey().$defaultFn(() => createId()),
     goalId: text('goal_id').references(() => goals.id).notNull(),   
     createdAt: timestamp('created_at', {withTimezone: true}).notNull().defaultNow(),
 })
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,34 @@
 import  fastifay from "fastify";
+import { serializerCompiler, validatorCompiler,type ZodTypeProvider } from "fastify-type-provider-zod";
+import { createGoal } from "../functions/create-goal";
+import z from 'zod'
+import { getWeekPendingGoals } from "../functions/get-week-pending-goals";
+import { createGoalCompletion } from "../functions/create-goal-completion";
 
-const app = fastifay()
+const app = fastifay().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.get('/pending-goals', async () => {
+    const {pendingGoals} = await getWeekPendingGoals()
+
+    return {pendingGoals}
+})
+
+app.post('/completions', {
+    schema: {
+        body: z.object({
+            goalId: z.string(),
+        }),
+    },
+}, async request =>{  
+    const { goalId} = request.body
+
+    await createGoalCompletion({
+        goalId,
+    })
+})
 
 app.listen({
     port: 3333
